ff1e5765c86208e93e0e1bff07a950256debfb2a
lburnet <75073111+aleyra@users.noreply.github.com>
2021-02-15 16:06:14 +0100

    Delete config-localhost.php

--- srcs/config-localhost.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME', 'wp_database');
-define('DB_USER', 'user');
-define('DB_PASSWORD', 'password');
-define('DB_HOST', 'localhost');
-define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
-?>
