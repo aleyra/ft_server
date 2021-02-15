d6981e79f4e46e5b01ab6de72d74176775abcd9b
lburnet <75073111+aleyra@users.noreply.github.com>
2021-02-15 16:05:53 +0100

    youpi

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -3,11 +3,9 @@ then
 	autoindex=on
 fi
 if [ $autoindex = "on" ]; then
-	sed -i "s/autoindex off/autoindex on/" ./etc/nginx/sites-enabled/nginx.conf;
-	service nginx restart
+	sed -i "s/autoindex off/autoindex on/" ./etc/nginx/sites-enabled/default;
 elif [ $autoindex = "off" ]; then
-	sed -i "s/autoindex on/autoindex off/" ./etc/nginx/sites-enabled/nginx.conf;
-	service nginx restart
+	sed -i "s/autoindex on/autoindex off/" ./etc/nginx/sites-enabled/default;
 else
 	echo "You need to use ./autoindex {on|off}"
 fi
